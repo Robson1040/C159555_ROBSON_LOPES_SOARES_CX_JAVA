9ebcc6aec8e4cdfae0f5613ed588ad24f268a29c
c159555 <c159555@corp.caixa.gov.br>
2025-11-15 17:44:11 -0300

    limpar

--- start_quarkus.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set "JAVA_HOME=C:\Program Files\Java\jdk-21.0.2"
-set "PATH=%JAVA_HOME%\bin;%PATH%"
-cd /d "C:\Users\c159555\IdeaProjects\trabalhoAdaQuarkus2-main\trabalhoAdaQuarkus2-main\API VEICULO SSO"
-mvn quarkus:dev
--- test_login.sh
+++
@@ -1,6 +0,0 @@
-curl -X POST http://localhost:8080/entrar \
-  -H "Content-Type: application/json" \
-  -d '{
-    "username": "admin",
-    "password": "123456"
-  }'
